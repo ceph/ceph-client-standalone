da3ee97e9c8f15314f8148f2f88b43626e9ac5d4
Sage Weil <sage@newdream.net>
2009-06-19 14:12:54 -0700

    foo

--- pull_from_linux.sh
+++ pull_from_linux.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 upstream=$1
-here=`pwd`
+subdir="fs/staging/ceph"
 
 last=`cat last_upstream_commit`
 
-echo "upstream is $upstream, here is $here"
+echo "upstream is $upstream"
 echo "last commit was $last"
 
 test -d /tmp/$$ && rm -r /tmp/$$
@@ -13,8 +13,24 @@ mkdir /tmp/$$
 
 pushd .
 cd $upstream
-git-format-patch --relative=fs/staging/ceph -o /tmp/$$ $last
+git-format-patch --relative=$subdir -o /tmp/$$ $last
+git_ver=`git-rev-parse HEAD 2>/dev/null`
 popd
 
-git-am /tmp/$$
+echo removing empty commits
+for f in `ls /tmp/$$`
+do
+    test -s /tmp/$$/$f || rm /tmp/$$/$f
+done
+
+echo applying commits
+for f in `ls -U /tmp/$$`
+do
+    git apply /tmp/$$/$f
+done
+
+rm -r /tmp/$$
+
+echo $git_ver > last_upstream_commit
+git commit -a -m "merged upstream through $git_ver"
 
